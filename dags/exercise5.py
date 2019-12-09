008a3f98c878b9efe934538478b9bdc2a1336a16
ahristov <ahristov@bol.com>
2019-12-09 14:22:58 +0100

    task5

--- dags/exercise5.py
+++ dags/exercise5.py
@@ -1,10 +1,9 @@
+import random
+
 import airflow
 from airflow.models import DAG
-from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
-from datetime import datetime, timedelta
 
 args = {
     'owner': 'Airflow',
@@ -12,36 +11,87 @@ args = {
 }
 
 dag = DAG(
-    dag_id='exercise5',
+    dag_id='example_branch_operator',
     default_args=args,
-    schedule_interval=None,
+    schedule_interval="@daily",
 )
 
-def print_weekday(**context):
-    week_day = context['execution_date'].weekday()
-    print(week_day)
-    return week_day
+run_this_first = DummyOperator(
+    task_id='run_this_first',
+    dag=dag,
+)
 
-
-t1 = PythonOperator(
-    task_id='print_weekday',
-    provide_context=True,
-    python_callable=print_weekday,
-    dag=dag)
+options = ['branch_a', 'branch_b', 'branch_c', 'branch_d']
 
 branching = BranchPythonOperator(
-        task_id='branching',
-        python_callable=print_weekday,
-        provide_context=True)
+    task_id='branching',
+    python_callable=lambda: random.choice(options),
+    dag=dag,
+)
+run_this_first >> branching
 
-t1 >> branching
-
-final_task = DummyOperator(
-    task_id='final_task',
+join = DummyOperator(
+    task_id='join',
     trigger_rule='one_success',
-    dag=dag)
+    dag=dag,
+)
 
-for i in range(0, 6):
-    email = DummyOperator(task_id='branch_{i}')
+for option in options:
+    t = DummyOperator(
+        task_id=option,
+        dag=dag,
+    )
 
-    branching >> email >> final_task
+    dummy_follow = DummyOperator(
+        task_id='follow_' + option,
+        dag=dag,
+    )
+
+    branching >> t >> dummy_follow >> join
+# import airflow
+# from airflow.models import DAG
+# from airflow.operators.bash_operator import BashOperator
+# from airflow.operators.dummy_operator import DummyOperator
+# from airflow.operators.python_operator import PythonOperator
+# from airflow.operators.python_operator import BranchPythonOperator
+# from datetime import datetime, timedelta
+#
+# args = {
+#     'owner': 'Airflow',
+#     'start_date': airflow.utils.dates.days_ago(2),
+# }
+#
+# dag = DAG(
+#     dag_id='exercise5',
+#     default_args=args,
+#     schedule_interval=None,
+# )
+#
+# def print_weekday(**context):
+#     week_day = context['execution_date'].weekday()
+#     print(week_day)
+#     return week_day
+#
+#
+# t1 = PythonOperator(
+#     task_id='print_weekday',
+#     provide_context=True,
+#     python_callable=print_weekday,
+#     dag=dag)
+#
+# branching = BranchPythonOperator(
+#         task_id='branching',
+#         python_callable=print_weekday,
+#         provide_context=True)
+#
+# t1 >> branching
+#
+# final_task = DummyOperator(
+#     task_id='final_task',
+#     trigger_rule='one_success',
+#     dag=dag)
+#
+# for i in range(0, 6):
+#     email = DummyOperator(task_id='branch_{i}')
+#
+#     branching >> email >> final_task
