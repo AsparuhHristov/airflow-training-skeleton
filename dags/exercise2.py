afc5f7927a73a4bf9198d7a520dc685e7ba40c09
ahristov <ahristov@bol.com>
2019-12-09 11:56:14 +0100

    second excercise b

--- dags/exercise2.py
+++ dags/exercise2.py
@@ -42,3 +42,69 @@ task5 = DummyOperator(
 )
 
 task1 >> task2  >> [task3, task4] >> task5
+
+dag2 = DAG(
+    dag_id='exercise2b',
+    default_args=args,
+    schedule_interval=[45 13 * * 1,3,5],
+)
+
+task1b = DummyOperator(
+    task_id='task1b',
+    dag=dag2,
+)
+
+task2b = DummyOperator(
+    task_id='task2b',
+    dag=dag2,
+)
+
+task3b = DummyOperator(
+    task_id='task3b',
+    dag=dag2,
+)
+
+task4b = DummyOperator(
+    task_id='task4b',
+    dag=dag2,
+)
+
+task5b = DummyOperator(
+    task_id='task5b',
+    dag=dag2,
+)
+
+task1b >> task2b  >> [task3b, task4b] >> task5b
+
+# dag3 = DAG(
+#     dag_id='exercise2c',
+#     default_args=args,
+#     schedule_interval='@daily',
+# )
+#
+# task1c = DummyOperator(
+#     task_id='task1c',
+#     dag=dag3,
+# )
+#
+# task2c = DummyOperator(
+#     task_id='task2c',
+#     dag=dag3,
+# )
+#
+# task3c = DummyOperator(
+#     task_id='task3c',
+#     dag=dag3,
+# )
+#
+# task4c = DummyOperator(
+#     task_id='task4c',
+#     dag=dag3,
+# )
+#
+# task5c = DummyOperator(
+#     task_id='task5c',
+#     dag=dag3,
+# )
+#
+# task1c >> task2c  >> [task3c, task4c] >> task5c
